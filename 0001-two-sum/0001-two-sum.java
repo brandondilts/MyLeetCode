5baa5b5d9f30d35531bcc2c62f9a1e892f345380
brandondilts <82127394+brandondilts@users.noreply.github.com>
2026-03-07 14:10:27 -0600

    Time: 35 ms (43.98%), Space: 47.1 MB (55.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int i=0;
+        int j;
+        int[] finalAnswer= new int[2];
+
+        while(i< nums.length){
+            j=i+1;
+
+            while((j) < nums.length) {
+                if (nums[i]+nums[j]==target){
+                    finalAnswer[0]= i;
+                    finalAnswer[1]= j;
+                    break;
+                } else{
+                    j++;
+                }
+
+                
+            }
+
+            i++;
+        }     
+        
+        return finalAnswer;
+    }
+
+}
